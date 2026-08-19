eae368f093c7428f1812f24526a4c3d07e3a2982
Nnenna Nnebe <franneci32@gmail.com>
2026-08-19 15:43:55 -0400

    Created a constructor for Tournament class, changed up JSON file to contain data for multiple tournaments

--- models/tournament.py
+++ models/tournament.py
@@ -2,11 +2,24 @@ import json
 from .player import Player
 
 class Tournament:
-    def __init__(self, name = None, filepath_tournament = None):
-       self.name = name
-       self.filepath_tournament = filepath_tournament
+    def __init__(self):
+        self.filepath_tournament = "data/tournaments/in-progress.json"
+        self.tournaments = {}
 
-    def save (self):
+        with open(self.filepath_tournament) as fp:
+            data = json.load(fp)
+            for d in data:
+                self.tournaments[d["name"]] = {
+                    "dates": d["dates"],
+                    "venue": d["venue"],
+                    "number_of_rounds": d["number_of_rounds"],
+                    "current_round": d["current_round"],
+                    "completed": d["completed"],
+                    "players": d["players"],
+                    "rounds": d["rounds"]
+                }
+
+    def save(self):
         pass
 
     def create_tournament (self, filepath_club):
