587a0d7412840ec6ac7b2f0751f4ab3eb4b70f11
Nnenna Nnebe <franneci32@gmail.com>
2026-08-19 16:59:04 -0400

    Completed register_player() method

--- models/tournament.py
+++ models/tournament.py
@@ -3,6 +3,9 @@ from .player import Player
 
 class Tournament:
     def __init__(self):
+        # An instance of Tournament class will create a dictionary of each tournament,
+        # with the tournament name as the key and the other data as values
+
         self.filepath_tournament = "data/tournaments/in-progress.json"
         self.tournaments = {}
 
@@ -20,19 +23,43 @@ class Tournament:
                 }
 
     def save(self):
-        pass
+        with open(self.filepath_tournament, "w") as fp:
+            json.dump(
 
-    def create_tournament (self, filepath_club):
-        pass
+            )
 
-    def register_player (self, filepath_club):
-        pass
+    def create_tournament (self, name, **kwargs):
+        self.tournaments[name] = {
+
+        }
+
+
+    def register_player (self, player_num, tournament_name, club_name):
+
+        try:
+            players = []
+            with open("data/clubs/" + club_name + ".json") as fp:
+                data = json.load(fp)
+
+                for d in data["players"]:
+                    players.append(d["chess_id"])
+
+                if player_num not in players:
+                    raise ValueError("Player not Found")
+                self.tournaments[tournament_name]["players"].append(player_num)
+
+        except FileNotFoundError:
+            print ("File not Found")
+
+        except ValueError as e:
+            print(e)
+
 
     def results (self):
-        pass
+        return self.tournaments
 
-    def advance (self):
-        pass
+    def advance (self, tournament_name):
+        self.tournaments[tournament_name]["current_round"] += 1
 
     def report (self):
         pass
